432107fddda52bd1d1919de3f1682221ef6fc1ad
David Calderon <David@DESKTOP-7UAHA94.(none)>
2017-08-04 22:54:20 -0700

    Initial Commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,9 @@
-
+var config = {
+  apiKey: "AIzaSyB0k0bLi8yKHWBZ9JrkglwxUPNBQD4rIvo",
+  authDomain: "train-scheduler-da38d.firebaseapp.com",
+  databaseURL: "https://train-scheduler-da38d.firebaseio.com",
+  projectId: "train-scheduler-da38d",
+  storageBucket: "train-scheduler-da38d.appspot.com",
+  messagingSenderId: "590642192993"
+};
+  firebase.initializeApp(config);
